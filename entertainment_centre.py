fe2f3ead9d884433cff57232aeb82dfcf7612884
Cubittz <paul.cubitt@gmail.com>
2016-11-17 14:41:39 +0000

    Add spacing to comments

--- entertainment_centre.py
+++ entertainment_centre.py
@@ -26,17 +26,17 @@ for m in in_cinemas.results:
     response = movie.info()
     # see if info has anything in the videos results list
     if (movie.videos()["results"]):
-        trailer_id = movie.videos()["results"][0]["key"] # if so, grab url for first video
+        trailer_id = movie.videos()["results"][0]["key"]  # if so, grab url for first video
     else:
-        trailer_id = "" # else leave trailer_id blank
+        trailer_id = ""  # else leave trailer_id blank
     
     # map movie info to an instance of class media.Movie    
-    new_item = media.Movie(movie.title.encode('cp1252') , #video_title
-                           movie.overview.encode('cp1252'), #plot
-                           base_url + movie.poster_path, #image_url 
-                           trailer_id, #trailer_id
-                           movie.runtime, #running_time
-                           movie.rating) #certificate
+    new_item = media.Movie(movie.title.encode('cp1252') ,  # video_title
+                           movie.overview.encode('cp1252'), # plot
+                           base_url + movie.poster_path,  # image_url 
+                           trailer_id,  # trailer_id
+                           movie.runtime,  # running_time
+                           movie.rating)  # certificate
     # add movie to movies array
     movies.append(new_item)
     
@@ -53,15 +53,15 @@ for t in top_rated.results:
     response = tv_show.info()
     # see if info has anything in the videos results list
     if (tv_show.videos()["results"]):
-        trailer_id = tv_show.videos()["results"][0]["key"] # if so, grab url for first video
+        trailer_id = tv_show.videos()["results"][0]["key"]  # if so, grab url for first video
     else:
-        trailer_id = "" # else leave trailer_id blank
+        trailer_id = ""  # else leave trailer_id blank
 
     # map tv_show info to an instance of class media.Tv_Show
-    new_item = media.Tv_Show(tv_show.original_name.encode('cp1252'), #video_title
-                             tv_show.overview.encode('cp1252'), #plot
-                             base_url + tv_show.poster_path, #image_url 
-                             trailer_id) #trailer_id
+    new_item = media.Tv_Show(tv_show.original_name.encode('cp1252'),  # video_title
+                             tv_show.overview.encode('cp1252'),  # plot
+                             base_url + tv_show.poster_path,  # image_url 
+                             trailer_id)  # trailer_id
     # add tv_show to tv_shows array
     tv_shows.append(new_item)
 # pass both arrays to open_movies_page function in fresh_tomatoes
